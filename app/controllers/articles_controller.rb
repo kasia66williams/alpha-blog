3e791c4a8d1b199c2cb8a0189ceb13d8d854c208
kasia66williams <kasia66williams@gmail.com>
2020-04-16 16:07:55 +0200

    new article features redirect_to'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,9 @@ def new
 
 end
 def create
-
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)
+  
 end    
 end    
