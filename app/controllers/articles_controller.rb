251bf276efcc6f5ea361b91909ba28f2c8e4cf04
kasia66williams <kasia66williams@gmail.com>
2020-04-17 19:21:34 +0200

    add _form and _message partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
 def show
-@article = Article.find(params[:id])
+
 end    
 def index
 @articles = Article.all
@@ -11,10 +13,10 @@ def new
 end
 
 def edit
-    @article = Article.find(params[:id])
+   
 end    
 def create
-@article = Article.new(params.require(:article).permit(:title, :description))
+@article = Article.new(article_params)
 if @article.save
     flash[:notice] = "Article was created successfully."
 redirect_to @article
@@ -24,8 +26,8 @@ end
 end 
 
 def update
-@article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."  
       redirect_to @article 
     else
@@ -34,10 +36,23 @@ def update
 end    
 
 def destroy
-    @article = Article.find(params[:id])
+  
     @article.destroy
     redirect_to articles_path
 
+
+end   
+
+private
+
+def set_article
+
+    @article = Article.find(params[:id])
+end
+
+def article_params
+
+    params.require(:article).permit(:title, :description)
 
 end    
 
